5cb7527c0bbdfa7402a64d49a598edd7da196691
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-12 19:30:41 +0530

    Dispatching Actions to Redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,8 +1,35 @@
 import { createStore } from "redux"
 
 // createStore (( FUNCTION WITH PROPS state = {default values}))
-const store = createStore((state = { count: 0 }) => {
-  return state
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + 1 }
+    case "DECREMENT":
+      return { count: state.count - 2 }
+    case "RESET":
+      return { count: 0 }
+
+    default:
+      return state
+  }
 })
+
+// Incrementing count.. Dispatching an action
+store.dispatch({
+  type: "INCREMENT"
+})
+
+// Reset Count
+store.dispatch({
+    type: "RESET"
+  })
+
+// Decreasing count.. Dispatching an action
+store.dispatch({
+  type: "DECREMENT"
+})
+
+
 
 console.log(store.getState())
