d52bbbf7161339c5f32e2ade8dea333883552c74
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-12 19:43:36 +0530

    Playground - Subscribing and Dynamic Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,9 +4,18 @@ import { createStore } from "redux"
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { count: state.count + 1 }
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1
+      return { count: state.count + incrementBy }
+
     case "DECREMENT":
-      return { count: state.count - 2 }
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1
+      return { count: state.count - decrementBy }
+
+    case "SET":
+      return { count: action.count }
+
     case "RESET":
       return { count: 0 }
 
@@ -15,21 +24,55 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 })
 
+// Suscribe is called each time the state has changed
+// Calling subscribe returns a function that can be
+// called to unsubscribe
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+})
+
 // Incrementing count.. Dispatching an action
+// Type is mandatory but can add any other key value pairs
+store.dispatch({
+  type: "INCREMENT",
+  incrementBy: 5
+})
+
 store.dispatch({
   type: "INCREMENT"
 })
 
 // Reset Count
 store.dispatch({
-    type: "RESET"
-  })
+  type: "RESET"
+})
+
+// Decreasing count.. Dispatching an action
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10
+})
 
 // Decreasing count.. Dispatching an action
 store.dispatch({
   type: "DECREMENT"
 })
 
+// Decreasing count.. Dispatching an action
+store.dispatch({
+  type: "DECREMENT"
+})
 
+// Sets count.. Dispatching an action
+store.dispatch({
+  type: "SET",
+  count: 101
+})
 
-console.log(store.getState())
+// Unsubscribe from the store
+unsubscribe()
+
+// Reset Count
+store.dispatch({
+  type: "RESET"
+})
