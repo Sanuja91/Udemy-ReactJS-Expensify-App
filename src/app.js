c0b3d8d41d3edfc1d6e8b913142bb86b4c75cbe8
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-12 15:55:05 +0530

    Organizing the Navigation routes

--- src/app.js
+++ src/app.js
@@ -1,57 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom"
-import "./styles/styles.scss"
-import "normalize.css/normalize.css"
+import AppRouter from "./routers/AppRouter"
 
-const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
-)
-
-const AddExpensePage = () => <div>This is from my add expense component</div>
-const EditExpensePage = () => <div>This is from my edit expense component</div>
-const HelpPage = () => <div>This is from my help page component</div>
-
-const NotFoundPage = () => (
-  <div>
-    Error 404! Page not found <Link to="/">Go Home</Link>
-  </div>
-)
-
-// Header with titel and Navigation
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active" exact={true}>
-      Create Expense
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active" exact={true}>
-      Edit Expense
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active" exact={true}>
-      Help
-    </NavLink>
-  </header>
-)
-
-// Pages for the website
-// Header added for every page
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById("app"))
+ReactDOM.render(<AppRouter />, document.getElementById("app"))
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom"
+import AddExpensePage from "../components/AddExpensePage"
+import ExpenseDashboardPage from "../components/ExpenseDashboardPage"
+import EditExpensePage from "../components/EditExpensePage"
+import HelpPage from "../components/HelpPage"
+import NotFoundPage from "../components/NotFoundPage"
+
+// Header with titel and Navigation
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active" exact={true}>
+      Create Expense
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active" exact={true}>
+      Edit Expense
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active" exact={true}>
+      Help
+    </NavLink>
+  </header>
+)
+
+// Pages for the website
+// Header added for every page
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+
+export default AppRouter
