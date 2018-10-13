2632893fc030a42de92ca2533d34a1059ddbe210
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-13 11:30:04 +0530

    Playground - Filtering Redux Data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -110,6 +110,21 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
   }
 }
 
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter(expense => {
+    const startDateMatch =
+      typeof startDate !== "number" || expense.createdAt >= startDate
+    const endDateMatch =
+      typeof endDate !== "number" || expense.createdAt <= endDate
+    const textMatch =
+      typeof text !== "string" ||
+      expense.description.toLowerCase().includes(text.toLowerCase())
+
+    return startDateMatch && endDateMatch && textMatch
+  })
+}
+
 // Store creation
 // Each route is managed by a specific reducer
 const store = createStore(
@@ -120,7 +135,9 @@ const store = createStore(
 )
 
 const unsubscribe = store.subscribe(() => {
-  console.log("STATE UPDATE", store.getState())
+  const state = store.getState()
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  console.log("STATE UPDATE", visibleExpenses)
 })
 
 const demoState = {
@@ -154,18 +171,18 @@ console.log({
   age: 27
 })
 
-// const expenseOne = store.dispatch(
-//   addExpense({ description: "Rent", amount: 100 })
-// )
-// const expenseTwo = store.dispatch(
-//   addExpense({ description: "Coffee", amount: 50 })
-// )
+const expenseOne = store.dispatch(
+  addExpense({ description: "Rent", amount: 100, createdAt: 1000 })
+)
+const expenseTwo = store.dispatch(
+  addExpense({ description: "Coffee", amount: 50, createdAt: -1000 })
+)
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
-// store.dispatch(setTextFilter("rent"))
+store.dispatch(setTextFilter("rent"))
 // store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 
-store.dispatch(setStartDate(125))
-store.dispatch(setEndDate(125333))
+// store.dispatch(setStartDate(125))
+// store.dispatch(setEndDate(1250))
