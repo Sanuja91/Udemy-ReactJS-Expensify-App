6137f647fc0f940f649f955567bc15976ccc2d4d
sanuja91 <sanuja.c@foodrunner.asia>
2018-10-13 17:48:45 +0530

    Adding Dropdown Menu for SortBy

--- src/app.js
+++ src/app.js
@@ -10,18 +10,12 @@ import "normalize.css/normalize.css"
 import "./styles/styles.scss"
 
 const store = configureStore()
-
+store.dispatch(addExpense({ description: "Gas bill", amount: 1000 }))
+store.dispatch(addExpense({ description: "Water bill", amount: 4500 }))
 store.dispatch(
-  addExpense({ description: "Water bill", amount: 500, createdAt: 1000 })
+  addExpense({ description: "Rent", amount: 109500, createdAt: 1000 })
 )
-store.dispatch(
-  addExpense({ description: "Gas bill", amount: 4500, createdAt: -1000 })
-)
-store.dispatch(setTextFilter("water"))
 
-setTimeout(() => {
-  store.dispatch(setTextFilter("bill"))
-}, 3000)
 
 const state = store.getState()
 const visibleExpenses = getVisbileExpenses(state.expenses, state.filters)
